b0d9dda845a9fd7b5f68874f032c8147cc4c2a07
DoubleSpicy <stven3au@gmail.com>
2023-09-17 03:32:11 +0800

    hook (WIP)

--- main.cpp
+++ main.cpp
@@ -63,13 +63,34 @@ LRESULT CALLBACK HookProc(
 		return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
+// https://stackoverflow.com/questions/16530871/findwindow-does-not-find-the-a-window
+{
+	char buffer[128];
+	int written = GetWindowTextA(hwnd, buffer, 128);
+	if (written && strstr(buffer, "Mozilla Firefox") != NULL) {
+		*(HWND*)lParam = hwnd;
+		return FALSE;
+	}
+	return TRUE;
+}
+
+HHOOK hKeyboardHook = 0;
+LRESULT CALLBACK KeyboardCallback(int code, WPARAM wParam, LPARAM lParam) {
+	cout << "a key was pressed" << endl;
+	//ofstream myfile;
+	//myfile.open("hookcheck.txt", ios::ate | ios::app);
+	//myfile << "a key was pressed\n";
+	//myfile.close();
+	return CallNextHookEx(hKeyboardHook, code, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
 	CoInitialize(0);
 	BYTE byte0 = 0;
 	BYTE byte1 = 0;
 	BYTE byte2 = 0;
 	log(string("START capturing inputs\nMode = " + input_mode + '\n').c_str());
-	
 	// detect your stick first
 	if (input_mode == "XInput") {
 		handleXInputGamePlay();
@@ -82,11 +103,25 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
 	}
 	// find pid of target application
 	//https://stackoverflow.com/questions/16530871/findwindow-does-not-find-the-a-window
-	HWND windowHandle = FindWindow(NULL, _T("Steam"));
+	string targetAppName = "Steam";
+	//log(string("search pid in pattern: " + targetAppName + '\n').c_str());
+	HWND windowHandle = FindWindowA(NULL, targetAppName.c_str());
 	DWORD threadId = GetWindowThreadProcessId(windowHandle, NULL);
 	log(string("thread id is: " + to_string(threadId) + '\n').c_str());
-	//HHOOK hook = ::SetWindowsHookEx(WH_CBT, HookCBTProc, hInst, threadId);
+	HHOOK hook = ::SetWindowsHookEx(WH_KEYBOARD, (HOOKPROC)KeyboardCallback, 0, threadId);
+	if (!hook) {
+	// https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes
+		log(string("Hook failed\n").c_str());
+		log(string("Error code: " + to_string(GetLastError()) + '\n').c_str());
+	}
+	else {
+		log(string("Hook successed!\n").c_str());
+	}
 
+	while (true) {
+
+	}
+	/*cin >> block;*/
 	CoUninitialize();
-
+	return WM_QUIT;
 }
